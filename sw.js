187461760aec0f1d84105c5b8b9540658f7dabac
edartiga-oss <edartiga@gmail.com>
2026-04-13 08:09:10 -0500

    Version 39

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* IH Field App — Service Worker v36 */
-const CACHE_NAME = 'ih-field-v36';
+/* IH Field App — Service Worker v37 */
+const CACHE_NAME = 'ih-field-v37';
 const ASSETS = ['./index.html', './', './sw.js'];
 
 self.addEventListener('install', e => {
